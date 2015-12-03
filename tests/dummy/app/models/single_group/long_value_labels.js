b55536f2abbc4fd3159afa028927acbc431b180e
Phil Nachum <phil.nachum@addepar.com>
2015-12-03 09:32:26 -0800

    Flush labels for horizontal bar charts with positive and negative values
    Closes #144

---
+++ tests/dummy/app/models/single_group/long_value_labels.js
@@ -0,0 +1,25 @@
+export default [{
+    label: "Label 1",
+    value: 1230981230,
+    type: "percent"
+  }, {
+    label: "Label 2",
+    value: 20912312,
+    type: "percent"
+  }, {
+    label: "Label 3",
+    value: 3212123,
+    type: "percent"
+  }, {
+    label: "Label 4",
+    value: -512019283,
+    type: "percent"
+  }, {
+    label: "Label 5",
+    value: 0,
+    type: "percent"
+  }, {
+    label: "Label 6",
+    value: -120938123,
+    type: "percent"
+}];
