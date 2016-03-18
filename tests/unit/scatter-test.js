f53845f8f7af2ec7fa8c740627147b6f2f6b7b8e
Thang Dinh <thang.dinh@addepar.com>
2016-03-18 14:51:07 -0700

    Fixed color on scatter chart legend
    Problem: When we assign the color override on each data dot of the
    scatter chart, it displays correctly for the data dot but not the legend
    
    Solution: Fixed the getGroupColor function to get the right color
    
    Tests are added.

--- tests/unit/scatter-test.js
+++ tests/unit/scatter-test.js
@@ -26,6 +26,21 @@ var scatterContent = {
   ]
 };
 
+var  overridingData = [
+  {
+    "group": "Energy",
+    "xValue": 0.017,
+    "yValue": 0.03,
+    "color": "#010101"
+  },
+  {
+    "group": "Industrial Metals",
+    "xValue": -0.28,
+    "yValue": -0.08
+  }
+];
+
+
 test("it exists", function(assert){
   assert.ok(this.subject());
 });
@@ -105,3 +120,45 @@ test('Test domain without domain padding', function(assert) {
   assert.equal(component.get('xScale')(0.017), 497.65);
   assert.equal(component.get('yScale')(0.03), 193.815);
 });
+
+test('Test legend color override', function(assert) {
+  var component = this.subject(scatterContent);
+  var firstLegentItem = component.get('legendItems')[0];
+
+  assert.equal(firstLegentItem.fill, '#414141',
+    'the fill color of legend item without color overriding is correct');
+  assert.equal(firstLegentItem.stroke, '#414141',
+    'the stroke color of legend item without color overriding is correct');
+
+  // Add overriding colors
+  Ember.run(function() {
+    component.set('data', overridingData);
+  });
+
+  firstLegentItem = component.get('legendItems')[0];
+  assert.equal(firstLegentItem.fill, '#010101',
+    'the fill color of legend item is overriden correctly');
+  assert.equal(firstLegentItem.stroke, '#010101',
+    'the stroke color of legend item is overriden correctly');
+});
+
+test('Test data point color override', function(assert) {
+  var component = this.subject(scatterContent);
+  var firstDataPoint = component.get('data')[0];
+  assert.equal(component.get('pointAttrs').fill(firstDataPoint,0), '#414141',
+    'the fill color of data point without color overriding is correct');
+  assert.equal(component.get('pointAttrs').stroke(firstDataPoint,0), '#414141',
+    'the stroke color of data point without color overriding is correct');
+
+  // Add overriding colors
+  Ember.run(function() {
+    component.set('data', overridingData);
+  });
+
+  firstDataPoint = component.get('data')[0];
+  assert.equal(component.get('pointAttrs').fill(firstDataPoint, 0), '#010101',
+    'the fill color of data point is overriden correctly');
+  assert.equal(component.get('pointAttrs').stroke(firstDataPoint, 0), '#010101',
+    'the stroke color of data point is overriden correctly');
+});
+
