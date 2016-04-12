b00ac35c97b82f380e7d95644401ce5d118c7c12
Daniel Xu <daniel.xu@addepar.com>
2016-04-12 12:40:14 -0700

    This commit prevents labels from intersecting one another within high density areas (#159)
    1) Fixes an existing bug with detectLabelOverlap.
    
    2) Pie chart labels will no longer intersect since the labels will move up / down
    along the pie until they find a "safe" location
    
    3) If the algorithm detects an extremely dense area of potential label intersections,
    it will rotate the pie to a "safe" orientation
    
    Added detectDenseSmallSlices function which improves the Pie Chart rotation algorithm
    
    Added comments, removed logging
    
    Adding new test data
    
    Added more test data + additional edge case logic for detectDenseSmallSlices
    
    Fixed a bug involving very large Other slices, added more data
    
    Tests pass + added comments
    
    pushing dist file
    
    Addressed Billy Littlefield + Thang comments
    
    adding grunt dist
    
    Added another test data file
    
    Fixing last comments

---
+++ tests/dummy/app/models/single_group/contains_zero.js
@@ -0,0 +1,14 @@
+export default [{
+    label: "Label 1",
+    value: 48
+  }, {
+    label: "Label 2",
+    value: 48
+  }, {
+    label: "Label 3",
+    value: 4
+  }, {
+    label: "Label 4",
+    value: 0
+  }];
+
