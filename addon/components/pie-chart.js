b2f415560d1a2c5c18368fad4c93ae76d552e7ae
Daniel Xu <daniel.xu@addepar.com>
2016-04-12 17:36:23 -0700

    Adding length check

--- addon/components/pie-chart.js
+++ addon/components/pie-chart.js
@@ -296,7 +296,11 @@ const PieChartComponent = ChartComponent.extend(FloatingTooltipMixin,
       return d.percent + p;
     }, 0);
 
-    if ((finishedData.length > 0) && detectDenseSmallSlices(finishedData)) {
+    if (finishedData.length == 0) {
+      return 0;
+    }
+
+    if (detectDenseSmallSlices(finishedData)) {
       return this.get('rotationOffset');
     } else {
       return _.last(finishedData).percent / sum * 2 * Math.PI;
