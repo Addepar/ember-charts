fa149c63a9012f7e7380fcbcedcf75006d3dad97
Stephen Hiller <stephen.hiller@addepar.com>
2016-07-25 14:13:58 -0700

    Revert "Refactoring Time Serries charts in an effort to standardize and simplify the"
    This reverts commit 809ca395e7e4243790c1901475a97aea90bc368c.

--- addon/components/time-series-chart.js
+++ addon/components/time-series-chart.js
@@ -58,12 +58,6 @@ const TimeSeriesChartComponent = ChartComponent.extend(LegendMixin,
   // Bar left offset, as fraction of width of bar
   barLeftOffset: 0.0,
 
-  // Force X-Axis labels to print vertically
-  xAxisVertLabels: false,
-
-  // Enable smart turning if feasible - TRUMPED by xAxisVertLabels
-  xAxisSmartLabels: false,
-
   // ----------------------------------------------------------------------------
   // Time Series Chart Constants
   // ----------------------------------------------------------------------------
@@ -830,7 +824,7 @@ const TimeSeriesChartComponent = ChartComponent.extend(LegendMixin,
         transform: "translate(0," + graphicTop + graphicHeight + ")"
       }).call(xAxis);
 
-    //tickSize draws the Y-axis allignment line across the whole of the graph.
+    //tickSize isn't doing anything here, it should take two arguments
     var yAxis = d3.svg.axis()
       .scale(this.get('yScale'))
       .orient('right')
