da3d2646d7673b4cdc3f12ae6915a366a5f84e32
Daniel Xu <daniel.xu@addepar.com>
2016-08-29 13:25:09 -0700

    Rounded Zero Percent Slice Configurations for Pie Charts (#181)
    `git cherry-pick 8b8b561fe788c4b156f1d04b9e78f4fe712ad4d2` from master to Addepar
    
    This branch fixes rounding errors and adds the missing Other Slice for when the Other Slice is incorrectly rounded to 0%. It also adds a toggle to allow users to include / exclude zero percent slices. These 0% slices respect slice ordering and number of decimal points. Finally, it contains tests for all of the above.

---
+++ tests/dummy/app/models/single_group/tiny_slice_two.js
@@ -0,0 +1,17 @@
+export default [{
+    label: "Label 1",
+    value: 25.3,
+    type: "percent"
+  }, {
+    label: "Label 2",
+    value: 74.4,
+    type: "percent"
+  }, {
+    label: "Label 3",
+    value: 0.1,
+    type: "percent"
+  }, {
+    label: "Label 4",
+    value: 0.2,
+    type: "percent"
+}];
